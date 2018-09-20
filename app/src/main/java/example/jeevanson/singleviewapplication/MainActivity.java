02e8d07b4104f6aa9140916ad1b971839169ca87
Jeevanson <jmahathuvam@encoress.com>
2018-09-20 10:57:10 +0530

    initial commit

---
+++ app/src/main/java/example/jeevanson/singleviewapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package example.jeevanson.singleviewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CustumView view = new CustumView(this);
+
+        setContentView(view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}

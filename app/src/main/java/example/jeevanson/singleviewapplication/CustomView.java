61e4995fe18ddaab35b0de7de5e463c9d46f25a4
Jeevanson <jmahathuvam@encoress.com>
2018-09-20 14:15:04 +0530

    Added color picker

--- app/src/main/java/example/jeevanson/singleviewapplication/CustomView.java
+++ app/src/main/java/example/jeevanson/singleviewapplication/CustomView.java
@@ -4,26 +4,48 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class CustumView extends View {
+public class CustomView extends View {
 
     Paint paint;
     float touchX;
     float touchY;
+    int backgroundColor;
 
-    public CustumView(Context context) {
+    public CustomView(Context context) {
         super(context);
+        init();
+    }
+
+    public void setViewBackground(int red, int green, int blue) {
+
+        backgroundColor = Color.rgb(red,green,blue);
+        invalidate();
+    }
+
+
+
+    public CustomView(Context context, AttributeSet attr) {
+        super(context,attr);
+        init();
+    }
+
+    private void init() {
         paint = new Paint();
         paint.setColor(Color.YELLOW);
+        setViewBackground(0,0,0);
     }
+
+
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        canvas.drawColor(Color.RED);
+        canvas.drawColor(backgroundColor);
         canvas.drawCircle(touchX,touchY,50,paint);
 
     }
